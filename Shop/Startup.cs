e12133d16c8354e4954f5e55572597e659900bc0
CmaillaKurup <cami670v@zbc.dk>
2022-03-08 10:49:02 +0100

    Sessionhåndtering tilføjet

--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -28,6 +28,10 @@ namespace Shop
         {
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Shop", Version = "v1"}); });
+            // Add MVC services to the services container.
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDistributedMemoryCache(); // Adds a default in-memory implementation of IDistributedCache
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +51,16 @@ namespace Shop
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            
+            // IMPORTANT: This session call MUST go before UseMvc()
+            app.UseSession();
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
